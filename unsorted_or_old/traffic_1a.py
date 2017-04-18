502c412dcd9496ae981fa9140722429eb20e2cc6
Dean Lin <richer6605@gmail.com>
2017-04-18 17:58:11 +0800

    Update traffic_1a.py

--- unsorted_or_old/traffic_1a.py
+++ unsorted_or_old/traffic_1a.py
@@ -10,5 +10,5 @@ client.connect('192.168.2.222', username = usr, password = pwd)
 while True:
   counttime = time.time()
   if counttime - starttime >= 60:
-    stdin, stdout, stderr = client.exec_command('PATH=$PATH:/usr/sbin; lilith -r all')
+    stdin, stdout, stderr = client.exec_command('PATH=$PATH:/usr/sbin; restart_command')
     starttime += 60
