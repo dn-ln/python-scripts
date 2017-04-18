ed458f8f9881b6ff3374644dce25468b00fe0b8d
Dean Lin <richer6605@gmail.com>
2017-04-18 17:54:23 +0800

    Update generateCurls_4a_unfinished.py

--- unsorted_or_old/generateCurls_4a_unfinished.py
+++ unsorted_or_old/generateCurls_4a_unfinished.py
@@ -8,8 +8,8 @@ from collections import OrderedDict
 json_data = {
 		"jumboId": "<jumboId>",
 		"roiId": "<roiId>",
-		"snapshot": "https://www.umbocv.com/images/page_learnc_01-2.07877134.png",
-		"previewVideo": "https://www.umbocv.com/images/page_learnc_01-2.07877134.png",
+		"snapshot": "https://www.xxx.com/images/page_learnc_01-2.07877134.png",
+		"previewVideo": "https://www.xxx.com/images/page_learnc_01-2.07877134.png",
 		"beginTime": "<timestamp>",
 		"objectType": "person"
 	    }
@@ -20,7 +20,7 @@ print(order_data)
 opt = ["-i", "-X", "-d", "-H"]
 req = ["POST"]
 hdr = ["Authorization: Bearer <token>", "Accept: application/json"]
-api = ["https://aqua-rc.umbocv-inc.com/api/alerts/cv"]
+api = ["https://xxx.com/api/alerts/cv"]
 
 
 print('curl', opt[0], opt[1], req[0], opt[2], order_data, opt[3], hdr[0], opt[3], hdr[1], api[0])
